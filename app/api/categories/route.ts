dc6a25a240885f4b701cc09766fa34b4576ba961
HeiPavel <112711905HeiPavel@users.noreply.github.com>
2024-01-24 16:06:25 +0200

    Simulate fetching categories in route handler

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -1,8 +1,10 @@
 export async function GET() {
-    const url = process.env.SERVER_URL;
+    //const baseUrl = process.env.SERVER_URL;
+    //const url = new URL('/', baseUrl);
     try {
-        const response = await fetch('url');
-        const data = await response.json();
+        //const response = await fetch(url);
+        //const data = await response.json();
+        const data = ['pizza', 'burgers', 'salads', 'desserts'];
         return Response.json(data);
     } catch(error) {
         console.log(error);
