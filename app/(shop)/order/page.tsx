e679af4722ac018365066fb060ed8a9e9bde013d
Homer <Home@fdasdar>
2024-03-01 11:41:27 +0200

    removed an extra request from the order page

--- app/(shop)/order/page.tsx
+++ app/(shop)/order/page.tsx
@@ -1,12 +1,9 @@
 import OrderPage from '@/components/orderPage/OrderPage';
 import styles from './page.module.scss';
-import { getCategories } from '@/components/header/navBar/NavBar';
 
 type Props = { params: {} };
 
 const Page = async (params: Props) => {
-    const { pages } = await getCategories();
-
     return <OrderPage />;
 };
 
