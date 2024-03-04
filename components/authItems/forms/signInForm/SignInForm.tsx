8b794297764189484969413fd7b45ce75bb584b4
GalSanyi <galsanyi84@gmail.com>
2024-03-04 16:08:10 +0100

    fixed password icon

--- components/authItems/forms/signInForm/SignInForm.tsx
+++ components/authItems/forms/signInForm/SignInForm.tsx
@@ -15,7 +15,7 @@ import ReactInputMask from 'react-input-mask';
 
 type Props = {};
 const initialState: ISignInFormState = { message: '', user: null };
-export default function SignInForm({}: Props) {
+export default function SignInForm({ }: Props) {
     const [state, formAction] = useFormState(login, initialState);
     const { signIn } = useAuth();
     const { replace } = useRouter();
@@ -43,7 +43,7 @@ export default function SignInForm({}: Props) {
     const validatePhoneNumber = () => {
         const phoneNumberExists = testPhone.some(item => item.number === phoneValue);
         if (!phoneNumberExists) {
-            setPhoneError('This phone number does not exist');
+            setPhoneError('');
         } else {
             setPhoneError('');
         }
@@ -52,7 +52,7 @@ export default function SignInForm({}: Props) {
     const validatePassword = () => {
         const passwordExist = testPassword.some(item => item.password === passwordValue);
         if (!passwordExist) {
-            setPasswordError('No such password exists');
+            setPasswordError('');
         } else {
             setPasswordError('');
         }
@@ -77,40 +77,61 @@ export default function SignInForm({}: Props) {
             signIn(state.user);
             replace(callbackUrl);
         }
-  }, [state, callbackUrl, signIn, replace]);
-  
+    }, [state, callbackUrl, signIn, replace]);
+
     const Visibility = (props: SvgIconProps) => (
-        <SvgIcon {...props} sx={{
-            // '&.MuiSvgIcon-root:hover path': {
-            //     stroke: '#F15C30',
-            // },
-            width: '38',
-            height: '38',
-        }}>
-            <svg width="38" height="38" viewBox="0 0 38 38" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M34.4375 19C34.4375 22.5625 27.5262 29.6875 19 29.6875C10.4738 29.6875 3.5625 22.5625 3.5625 19C3.5625 15.4375 10.4738 8.3125 19 8.3125C27.5262 8.3125 34.4375 15.4375 34.4375 19Z" stroke="#3E3B3B" stroke-width="2" stroke-linejoin="round" />
-                <path d="M24.9375 19C24.9375 20.5747 24.3119 22.0849 23.1984 23.1984C22.0849 24.3119 20.5747 24.9375 19 24.9375C17.4253 24.9375 15.9151 24.3119 14.8016 23.1984C13.6881 22.0849 13.0625 20.5747 13.0625 19C13.0625 17.4253 13.6881 15.9151 14.8016 14.8016C15.9151 13.6881 17.4253 13.0625 19 13.0625C20.5747 13.0625 22.0849 13.6881 23.1984 14.8016C24.3119 15.9151 24.9375 17.4253 24.9375 19Z" stroke="#3E3B3B" stroke-width="2" stroke-linejoin="round" />
-                <path d="M3 33.1035L34.2121 5.00003" stroke="#3E3B3B" stroke-width="2" stroke-linecap="round" />
+        <SvgIcon fontSize="large" {...props}>
+            <svg
+                width="38"
+                height="38"
+                viewBox="0 0 38 38"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path
+                    d="M34.4375 19C34.4375 22.5625 27.5262 29.6875 19 29.6875C10.4738 29.6875 3.5625 22.5625 3.5625 19C3.5625 15.4375 10.4738 8.3125 19 8.3125C27.5262 8.3125 34.4375 15.4375 34.4375 19Z"
+                    stroke="#040705"
+                    strokeWidth="2"
+                    strokeLinejoin="round"
+                />
+                <path
+                    d="M24.9375 19C24.9375 20.5747 24.3119 22.0849 23.1984 23.1984C22.0849 24.3119 20.5747 24.9375 19 24.9375C17.4253 24.9375 15.9151 24.3119 14.8016 23.1984C13.6881 22.0849 13.0625 20.5747 13.0625 19C13.0625 17.4253 13.6881 15.9151 14.8016 14.8016C15.9151 13.6881 17.4253 13.0625 19 13.0625C20.5747 13.0625 22.0849 13.6881 23.1984 14.8016C24.3119 15.9151 24.9375 17.4253 24.9375 19Z"
+                    stroke="#040705"
+                    strokeWidth="2"
+                    strokeLinejoin="round"
+                />
             </svg>
-
-
         </SvgIcon>
     );
 
     const VisibilityOff = (props: SvgIconProps) => (
-        <SvgIcon {...props} sx={{
-            // '&.MuiSvgIcon-root:hover path': {
-            //     stroke: '#F15C30',
-            // },
-            width: '38',
-            height: '38',
-        }}>
-            <svg width="38" height="38" viewBox="0 0 34 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M32.4375 12C32.4375 15.5625 25.5262 22.6875 17 22.6875C8.47375 22.6875 1.5625 15.5625 1.5625 12C1.5625 8.4375 8.47375 1.3125 17 1.3125C25.5262 1.3125 32.4375 8.4375 32.4375 12Z" stroke="#3E3B3B" stroke-width="2" stroke-linejoin="round" />
-                <path d="M22.9375 12C22.9375 13.5747 22.3119 15.0849 21.1984 16.1984C20.0849 17.3119 18.5747 17.9375 17 17.9375C15.4253 17.9375 13.9151 17.3119 12.8016 16.1984C11.6881 15.0849 11.0625 13.5747 11.0625 12C11.0625 10.4253 11.6881 8.91505 12.8016 7.80155C13.9151 6.68806 15.4253 6.0625 17 6.0625C18.5747 6.0625 20.0849 6.68806 21.1984 7.80155C22.3119 8.91505 22.9375 10.4253 22.9375 12Z" stroke="#3E3B3B" stroke-width="2" stroke-linejoin="round" />
+        <SvgIcon fontSize="large" {...props}>
+            <svg
+                width="38"
+                height="38"
+                viewBox="0 0 38 38"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path
+                    d="M34.4375 19C34.4375 22.5625 27.5262 29.6875 19 29.6875C10.4738 29.6875 3.5625 22.5625 3.5625 19C3.5625 15.4375 10.4738 8.3125 19 8.3125C27.5262 8.3125 34.4375 15.4375 34.4375 19Z"
+                    stroke="#3E3B3B"
+                    strokeWidth="2"
+                    strokeLinejoin="round"
+                />
+                <path
+                    d="M24.9375 19C24.9375 20.5747 24.3119 22.0849 23.1984 23.1984C22.0849 24.3119 20.5747 24.9375 19 24.9375C17.4253 24.9375 15.9151 24.3119 14.8016 23.1984C13.6881 22.0849 13.0625 20.5747 13.0625 19C13.0625 17.4253 13.6881 15.9151 14.8016 14.8016C15.9151 13.6881 17.4253 13.0625 19 13.0625C20.5747 13.0625 22.0849 13.6881 23.1984 14.8016C24.3119 15.9151 24.9375 17.4253 24.9375 19Z"
+                    stroke="#3E3B3B"
+                    strokeWidth="2"
+                    strokeLinejoin="round"
+                />
+                <path
+                    d="M3 33.1035L34.2121 5.00003"
+                    stroke="#3E3B3B"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                />
             </svg>
-
-
         </SvgIcon>
     );
     return (
@@ -122,7 +143,7 @@ export default function SignInForm({}: Props) {
                 gap: '24px',
             }}
         >
-        
+
             <ReactInputMask mask="+1 (999) 999 9999" value={phoneValue}
                 onChange={handlePhoneChange}>
                 <TextField type="text" name="phoneNumber" placeholder="Phone number"
@@ -203,10 +224,10 @@ export default function SignInForm({}: Props) {
                 label="Password"
             />
             {passwordError ? <FormHelperText error>{passwordError}</FormHelperText> : ''}
-            
-        {/*{state?.message !== 'Ok' && <FormHelperText>{state?.message}</FormHelperText>}*/}
-        {/*<SubmitButton isFormInvalid={false} text="Sign In" />*/}
-            
+
+            {/*{state?.message !== 'Ok' && <FormHelperText>{state?.message}</FormHelperText>}*/}
+            {/*<SubmitButton isFormInvalid={false} text="Sign In" />*/}
+
             <Button type="submit"
                 disabled={!phoneValue || !passwordValue || phoneError || passwordError ? true : undefined}
                 sx={{
@@ -226,6 +247,6 @@ export default function SignInForm({}: Props) {
                         }
                     }
                 }}>Log in </Button>
-           </Box >
+        </Box >
     );
 }
